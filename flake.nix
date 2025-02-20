50701ca3e0684af6a7d4dabb4a0e2fd198390f28
akuzihs <>
2025-02-20 13:58:44 +0400

    Replace flake-utils with flake-parts

--- flake.nix
+++ flake.nix
@@ -1,26 +1,27 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    systems.url = "github:nix-systems/default";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [
-            dos2unix
-            fd
-            git
-            git-lfs
-            go
-            jq
-            moreutils
-          ];
-        };
-      }
-    );
+  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+    systems = import inputs.systems;
+    perSystem = { pkgs, ... }: {
+      devShells.default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          dos2unix
+          fd
+          git
+          git-lfs
+          go
+          jq
+          moreutils
+        ];
+      };
+    };
+  };
 }
